99b3519f57c8b82946f19c893c9e05839c58d957
rem_w <cgrem78@yandex.ru>
2023-05-22 17:21:02 +0300

    base path

--- objects.cpp
+++ objects.cpp
@@ -1,14 +1,17 @@
 #include "objects.h"
 #include <QDebug>
+#include <QDir>
 namespace obj_r {
 // test db
 void read_db(){
     qDebug() << "started!";
     sqlite3 *db = 0;
-    sqlite3_open_v2("D:\\CODING\\C\\WIP\\refsystem\\db\\db.db", &db, SQLITE_OPEN_READWRITE, NULL);
+    sqlite3_open_v2("..\\refsystem\\db\\db.db", &db, SQLITE_OPEN_READWRITE, NULL);
     int rc;
     sqlite3_stmt* result;
     rc = sqlite3_prepare(db, "SELECT * FROM categories", -1, &result, NULL);
+    QDir dir;
+    qDebug() << dir.currentPath() << flush;
     qDebug() << "rc " << sqlite3_errmsg(db);
     if (rc == SQLITE_OK) {
         qDebug() << "base connected!";
